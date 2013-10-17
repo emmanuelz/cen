eac16b74e12e7c45b263444b7d17707342dc4ddf
Emmanuel ZURMELY <emmanuelz@users.sourceforge.net>
2013-10-17 22:33:39 +0200

    - redimensionnement automatique de Gameboard

--- src/org/cen/ui/gameboard/GameBoardView.java
+++ src/org/cen/ui/gameboard/GameBoardView.java
@@ -2,28 +2,49 @@ package org.cen.ui.gameboard;
 
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
 
 import javax.swing.JPanel;
 
 import org.cen.cup.cup2014.gameboard.GameBoard2014;
 
-public class GameBoardView extends JPanel {
+public class GameBoardView extends JPanel implements ComponentListener {
 	private static final long serialVersionUID = 1L;
 	private GameBoardPainter painter;
+
+	public GameBoardView() {
+		super();
+		addComponentListener(this);
+		painter = new GameBoardPainter();
+		GameBoard2014 gameBoard = new GameBoard2014();
+		painter.setGameBoard(gameBoard);
+	}
+
+	@Override
+	public void componentHidden(ComponentEvent e) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void componentMoved(ComponentEvent e) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void componentResized(ComponentEvent e) {
+		Dimension size = getSize();
+		painter.setSize(size);
+	}
+
+	@Override
+	public void componentShown(ComponentEvent e) {
+		// TODO Auto-generated method stub
+	}
 
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		painter.paint(g);
-	}
-
-	public GameBoardView() {
-		super();
-		Dimension d = new Dimension(640, 1280);
-		setPreferredSize(d);
-		painter = new GameBoardPainter();
-		GameBoard2014 gameBoard = new GameBoard2014();
-		painter.setGameBoard(gameBoard);
-		painter.setSize(d);
 	}
 }
